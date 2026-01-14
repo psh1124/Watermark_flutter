d25bbfe0886f184455b872a04d486966e5755270
psh1124 <a01038818368@gmail.com>
2026-01-14 13:58:13 +0900

    안드로이드 설정: Manifest 및 MainActivity 수정

--- android/app/src/main/kotlin/com/example/waterpark_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/waterpark_app/MainActivity.kt
@@ -1,5 +1,32 @@
-package com.example.waterpark_app
+package com.example.waterpark_app  // 자신의 패키지명으로 변경
 
+import android.os.Bundle
+import android.media.MediaScannerConnection
+import android.net.Uri
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "media_scanner"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "scanFile") {
+                val path = call.argument<String>("path")
+                if (path != null) {
+                    MediaScannerConnection.scanFile(context, arrayOf(path), null) { _, uri ->
+                        println("Scanned $path -> $uri")
+                    }
+                    result.success(null)
+                } else {
+                    result.error("INVALID_PATH", "Path is null", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
